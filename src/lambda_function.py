93c6a515bcc2a9db7cc3608ffb333ab7d6b20884
Tom Saleeba <tom.saleeba@gmail.com>
2019-06-06 14:04:11 +0930

    feat: fix up logic to work on 5 minute schedule

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,51 +1,69 @@
 import os
-from datetime import datetime as dt
+from datetime import datetime as dt, timedelta
 from dateutil import tz
 import ephem
 import requests
 
+TIME_FORMAT_STR = '%02d:%02d'
 FROM_ZONE = tz.gettz('UTC')
 TO_ZONE = tz.gettz('Australia/Adelaide')
 ADELAIDE_LAT = '-34.9786554'
 ADELAIDE_LONG = '138.5487406'
 
-# FIXME change CRON schedule to run every 5 minutes between times that sunset can happen
-# FIXME case input to int
-run_frequency = os.getenv('RUN_FREQUENCY', default=5)
+try:
+    run_frequency_minutes = int(os.getenv('RUN_FREQUENCY', default=5))
+except ValueError as e:
+    print('[ERROR] RUN_FREQUENCY env var was not an int')
+    raise e
 
 
 def is_closest_time(now_datetime, sunset_datetime):
-    # pass sunset as datetime obj
-    # create now as datetime obj (with tz)
-    # define schedule regularity (5 minutes)
-    # create datetime obj for next run
-    # check if now and next are either side of sunset, bail if not
-    # check if delta between now and sunset is less than next and sunset
-    # if now, then push, return
-    # check now and previous time
-    if now.hour < hour:
+    diff_now_seconds = abs((sunset_datetime - now_datetime).total_seconds())
+    run_frequency_seconds = run_frequency_minutes * 60
+    now_is_not_within_run_frequency_of_sunset = abs(
+        diff_now_seconds) > run_frequency_seconds
+    if now_is_not_within_run_frequency_of_sunset:
+        print('Now (%s) is more than %d minutes away from sunset (%s)' %
+              (str(now_datetime), run_frequency_minutes, str(sunset_datetime)))
         return False
-    if now.hour == hour:
-        return minute <= 30
-    if now.hour == hour + 1:
-        return minute > 30
-    return False
+    run_freq_minutes_delta = timedelta(minutes=run_frequency_minutes)
+
+    def before_sunset_strategy():
+        next_check_datetime = now_datetime + run_freq_minutes_delta
+        diff_next_seconds = abs(
+            (next_check_datetime - sunset_datetime).total_seconds())
+        now_is_closer_than_next = diff_now_seconds <= diff_next_seconds
+        return now_is_closer_than_next
+
+    def after_sunset_strategy():
+        prev_check_datetime = now_datetime - run_freq_minutes_delta
+        diff_prev_seconds = abs(
+            (sunset_datetime - prev_check_datetime).total_seconds())
+        now_is_closer_than_prev = diff_now_seconds < diff_prev_seconds
+        return now_is_closer_than_prev
+
+    is_before_sunset = now_datetime < sunset_datetime
+    if is_before_sunset:
+        return before_sunset_strategy()
+    return after_sunset_strategy()
 
 
 def now_time_str():
     now = dt.now(tz=TO_ZONE)
-    return '%s:%s' % (now.hour, now.minute)
+    return TIME_FORMAT_STR % (now.hour, now.minute)
 
 
 def lambda_handler(event, context):
     adelaide = get_observer()
+    # FIXME is next setting actually *next* or today?
     ephem_next_sunset = adelaide.next_setting(ephem.Sun())
     zoneless_next_sunset = ephem_next_sunset.datetime()
     next_sunset = zoneless_next_sunset.replace(tzinfo=FROM_ZONE)
     local_next_sunset = next_sunset.astimezone(TO_ZONE)
     decomposed = decompose(local_next_sunset)
-    sunset_time_str = '%s:%s' % (decomposed['hour'], decomposed['minute'])
-    if not is_closest_hour(decomposed['hour'], decomposed['minute']):
+    sunset_time_str = TIME_FORMAT_STR % (decomposed['hour'],
+                                         decomposed['minute'])
+    if not is_closest_time(dt.now(tz=TO_ZONE), local_next_sunset):
         print('[INFO] now (%s) is not closest hour to sunset (%s), bailing' %
               (now_time_str(), sunset_time_str))
         return 'Done, nothing sent'
--- test/test_lambda_function.py
+++ test/test_lambda_function.py
@@ -1,16 +1,66 @@
 import unittest
-from src.lambda_function import is_closest_hour
+from datetime import datetime as dt
+
+from src.lambda_function import is_closest_time, TO_ZONE
 
 
 class TestLambdaFunction(unittest.TestCase):
     def test_is_closest_hour01(self):
-        """ test if we can tell we're the closest time from before """
-        now = # FIXME
-        sunset = # FIXME
+        """ can tell we're the closest time from before """
+        now = dt(2019, 1, 25, 19, 35, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 37, 0, 0, TO_ZONE)
         result = is_closest_time(now, sunset)
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertTrue(result, 'Should be considered closest "before" time')
 
-    # test closest after
+    def test_is_closest_hour02(self):
+        """ can tell we're NOT the closest time from after """
+        now = dt(2019, 1, 25, 19, 40, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 37, 0, 0, TO_ZONE)
+        result = is_closest_time(now, sunset)
+        self.assertFalse(result,
+                         'Should NOT be considered closest "after" time')
 
-    # test not closest before
-    # test not closest after
+    def test_is_closest_hour03(self):
+        """ can tell we're the closest time from after """
+        now = dt(2019, 1, 25, 19, 40, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 38, 0, 0, TO_ZONE)
+        result = is_closest_time(now, sunset)
+        self.assertTrue(result, 'Should be considered closest "after" time')
+
+    def test_is_closest_hour04(self):
+        """ can tell we're NOT the closest time from before """
+        now = dt(2019, 1, 25, 19, 35, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 38, 0, 0, TO_ZONE)
+        result = is_closest_time(now, sunset)
+        self.assertFalse(result,
+                         'Should NOT be considered closest "before" time')
+
+    def test_is_closest_hour05(self):
+        """ can tell we're more than one run away on the before side """
+        now = dt(2019, 1, 25, 19, 30, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 38, 0, 0, TO_ZONE)
+        result = is_closest_time(now, sunset)
+        self.assertFalse(result, 'Should be considered too far away')
+
+    def test_is_closest_hour06(self):
+        """ can tell we're more than one run away on the after side """
+        now = dt(2019, 1, 25, 19, 45, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 38, 0, 0, TO_ZONE)
+        result = is_closest_time(now, sunset)
+        self.assertFalse(result, 'Should be considered too far away')
+
+    def test_is_closest_hour07(self):
+        """ can we deal with an equal diff from before """
+        now = dt(2019, 1, 25, 19, 35, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 37, 30, 0, TO_ZONE)
+        result = is_closest_time(now, sunset)
+        self.assertTrue(
+            result,
+            'Should be considered the closest (favour before in equal diff)')
+
+    def test_is_closest_hour08(self):
+        """ can we deal with an equal diff from after """
+        now = dt(2019, 1, 25, 19, 40, 0, 0, TO_ZONE)
+        sunset = dt(2019, 1, 25, 19, 37, 30, 0, TO_ZONE)
+        result = is_closest_time(now, sunset)
+        self.assertFalse(result, 'Should NOT be considered the closest')
